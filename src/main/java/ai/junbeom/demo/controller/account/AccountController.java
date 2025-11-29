ce84adcfa6e5aa9fbb8cccbb1f029e2b92971370
jbpark <qufkr1029@gmail.com>
2025-11-30 01:21:03 +0900

    - devtools 의존성 추가 및 thymeleaf cache 값 false 설정 - login.html 페이지 ui 변경

--- src/main/java/ai/junbeom/demo/controller/account/AccountController.java
+++ src/main/java/ai/junbeom/demo/controller/account/AccountController.java
@@ -1,10 +1,11 @@
 package ai.junbeom.demo.controller.account;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.ui.Model;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -13,14 +14,14 @@ import java.time.format.DateTimeFormatter;
 @Slf4j
 public class AccountController {
 
-    @GetMapping("/login")
+    @GetMapping({"/", "/login"})
     public String login(HttpServletRequest request, Model model) {
-        log.trace("Trace 메시지: 디테일한 실행 흐름");
-        log.debug("Debug 메시지: 개발 중 확인용 값 = {}", 22);
-        log.info("Info 메시지: 서버가 정상적으로 시작됨 : {}", 32);
-        log.warn("Warn 메시지: 캐시가 초기화되지 않았습니다. : {}", 44);
-        log.error("Error 메시지: DB 연결 실패 : {}", 22);
-        log.info("Log 클래스는 무엇이냐면 : {}", log.getClass().getName());
+        // log.trace("Trace 메시지: 디테일한 실행 흐름");
+        // log.debug("Debug 메시지: 개발 중 확인용 값 = {}", 22);
+        // log.info("Info 메시지: 서버가 정상적으로 시작됨 : {}", 32);
+        // log.warn("Warn 메시지: 캐시가 초기화되지 않았습니다. : {}", 44);
+        // log.error("Error 메시지: DB 연결 실패 : {}", 22);
+        // log.info("Log 클래스는 무엇이냐면 : {}", log.getClass().getName());
 
         // Get client IP address
         String clientIp = request.getRemoteAddr();
@@ -34,6 +35,6 @@ public class AccountController {
         model.addAttribute("clientIp", clientIp);
         model.addAttribute("requestTime", formattedNow);
 
-        return "login";
+        return "account/login";
     }
 }
